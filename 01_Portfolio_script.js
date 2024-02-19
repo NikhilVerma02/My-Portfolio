4a58cbe882ebb3ab3c587a906dd2f3e621cd2c34
NikhilVerma02 <nv6306887@gmail.com>
2024-02-19 23:20:07 +0530

    first commit

---
+++ 01_Portfolio_script.js
@@ -0,0 +1,74 @@
+// toggle style switcher
+
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// hide style switcher on scroll
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+// Theme Colors
+const alternatestyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+  alternatestyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+// Theme datk and light
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
+
+// ---------------- Aside ----------------------
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+  });
+}
+
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
